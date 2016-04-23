18bfb42965c96029d672e654dbd4ad85048b4c38
pham tuan <phamtuanchip@gmail.com>
2016-04-24 02:30:53 +0700

    update web

---
+++ java/ptbeauty/src/com/dao/SqliteDaoImpl.java
@@ -0,0 +1,48 @@
+package com.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.domain.Employee;
+
+public class SqliteDaoImpl implements DataDao {
+
+	 
+
+	@Override
+	@Transactional
+	public int insertRow(Employee employee) {
+		 
+		return (Integer) 1;
+	}
+
+	@Override
+	public List<Employee> getList() {
+		 
+		List<Employee> employeeList = new ArrayList<Employee>();
+				 
+		return employeeList;
+	}
+
+	@Override
+	public Employee getRowById(int id) {
+		 
+		Employee employee = new Employee();
+		return employee;
+	}
+
+	@Override
+	public int updateRow(Employee employee) {
+		 
+		return (Integer) 1;
+	}
+
+	@Override
+	public int deleteRow(int id) {
+		 
+		return (Integer) 1;
+	}
+
+}
